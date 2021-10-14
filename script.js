726dcce8a68c4b562b5eeb78768a469df20d39cc
supakit223 <supakit223@gmail.com>
2021-10-14 18:29:37 +0700

    ok

--- script.js
+++ script.js
@@ -3,38 +3,40 @@ function cal() {
     const result = getPermutation(numberArray)        
     let number = ""
     let count = 0
-    const sum = document.getElementById("sum").value
-        
+    const sum = document.getElementById("sum").value        
     for (let i = 0 ; i < 362880 ; i++) { 
         if(result[i][0]+result[i][1]+result[i][2] == sum && result[i][3]+result[i][4]+result[i][5] == sum && result[i][6]+result[i][7]+result[i][8] == sum
         && result[i][0]+result[i][3]+result[i][6] == sum && result[i][1]+result[i][4]+result[i][7] == sum && result[i][2]+result[i][5]+result[i][8] == sum
         && result[i][0]+result[i][4]+result[i][8] == sum && result[i][2]+result[i][4]+result[i][6] == sum){                 
-            // number = number + `<div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="1" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="00">
-            //     <input id="2" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="01">
-            //     <input id="3" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="02">
-            // </div>
-            // <div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="4" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="03">
-            //     <input id="5" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="04">
-            //     <input id="6" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="05">
-            // </div>
-            // <div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="7" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="06">
-            //     <input id="8" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="07">
-            //     <input id="9" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="08">
-            // </div>`
-            for(let j = 0 ; j < 9 ; j++){
                 count++
-                number = number + result[i][j] +"     "
-                if (count%3 == 0){
-                    number = number + "<br>"
-                }       
-                if (count%9 == 0){
-                    number = number + "<br>"
-                }                        
-            }            
-        }     
+                number = number + 
+                `
+                <div class="d-flex justify-content-center align-content-center">
+                    <label class="groupResult">ชุดที่ ${count}</label>
+                </div>
+                <div class="d-flex justify-content-center align-content-center">
+                    <div class="mt-3 d-flex align-content-center justify-content-center">
+                        <input id="1" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][0]}">
+                        <input id="2" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][1]}">
+                        <input id="3" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][2]}">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center align-content-center">
+                    <div class="mt-3 d-flex align-content-center justify-content-center">
+                        <input id="4" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][3]}">
+                        <input id="5" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][4]}">
+                        <input id="6" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][5]}">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center align-content-center">
+                    <div class="mt-3 d-flex align-content-center justify-content-center">
+                        <input id="7" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][6]}">
+                        <input id="8" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][7]}">
+                        <input id="9" class="p-1 mx-2 input" type="number" min="1" max="9" value="${result[i][8]}">
+                    </div>
+                </div><br>
+                `                       
+        }           
     }
     const inpObj = document.getElementById("sum")
     if (!inpObj.checkValidity()) {
@@ -64,7 +66,7 @@ function cal() {
         document.getElementById("alert").classList.remove("visible")
         document.getElementById("alert").classList.add("invisible")
 
-        document.getElementById("output1").innerHTML = `มี ${count/9} ชุดคำตอบ ดังนี้`
+        document.getElementById("output1").innerHTML = `มี ${count} ชุดคำตอบ ดังนี้`
         document.getElementById("output2").innerHTML = number
     }    
 }
